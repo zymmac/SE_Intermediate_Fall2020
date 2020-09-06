5dc931e9efd9473726e1e69a16470f533cb0e02d
ZymmAc <zymmac@gmail.com>
2020-09-06 19:45:27 -0300

    Cloned to Visual Studio and edited

--- chapter_3/3_10.py
+++ chapter_3/3_10.py
@@ -1,4 +1,6 @@
-# 3-10. Every Function: Think of something you could store in a list. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items and then uses each function introduced in this chapter at least once.
+# 3-10. Every Function: Think of something you could store in a list. 
+#   For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. 
+#   Write a program that creates a list containing these items and then uses each function introduced in this chapter at least once.
 
 languages = ['javascript', 'python', 'java', 'php']
 
