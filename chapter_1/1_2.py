8710ddc77a199414a2a4f65c3acd0746e5f86919
Alexandre Costa <alexandre.costa@code.berlin>
2020-09-06 19:33:26 +0000

    Starting chapter 2

---
+++ chapter_1/1_2.py
@@ -0,0 +1,2 @@
+# typo in the function results and error: printer("Hello Python Interpreter!")
+print("Henlo World") #typo in the string doesn't result in error
---
+++ chapter_1/1_3.py
@@ -0,0 +1,7 @@
+## My own console games emulator, with a kickass library
+
+## A cool portfolio webpage 
+
+## An app that would make a map of the floor and paint the wi-fi strength of each space 
+
+##Etc
